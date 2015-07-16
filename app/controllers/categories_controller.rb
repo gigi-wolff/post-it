26eb46b818967f86280aae8852e7bbe3c4f2bcb1
Gigi Wolff <gigi.wolff@gmail.com>
2015-07-16 19:11:50 -0400

    problem with Category Routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+ 
+  def new
+    @category = Category.new 
+  end
+ 
+  def create  
+    @category = Category.new(category_params)
+
+    if @category.save 
+      flash[:notice] = "Your category was created"
+      redirect_to root_path #goes to index action    
+    else
+      render :new #display new.html.erb
+    end
+  end
+
+  def show  
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)    
+  end
+
+end
