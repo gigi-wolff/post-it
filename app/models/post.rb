ac94a3c3a98ba7dc025d7e3b380d97819cbb7e22
Gigi Wolff <gigi.wolff@gmail.com>
2015-08-26 11:50:51 -0400

    improved slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -35,8 +35,32 @@ class Post < ActiveRecord::Base
 
 
   def generate_slug
-    self.slug = self.title.gsub(' ','-').downcase
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_count(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
   end
+
+  def append_count(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/,"-"
+    str.downcase
+  end
+
 
   #to_param is called automatically when post object is passed to path.
   #in _post.erb the link to comments line:
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,38 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   #"on: :create" means password must be present whenever a user object is created
   #therefore this condition will not be invoked for updates of existing user object
-  validates :password, presence: true, on: :create, on: :update, length: {minimum: 6}
+  validates :password, presence: true, on: :create, length: {minimum: 6}
 
   before_save :generate_slug
 
   def generate_slug
-    self.slug = self.username.gsub(' ','-').downcase
+    the_slug = to_slug(self.username)
+    user = User.find_by slug: the_slug
+    count = 2
+    while user && user != self
+      the_slug = append_count(the_slug, count)
+      user = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
   end
+
+  def append_count(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/,"-"
+    str.downcase
+  end
+
+
 
   def to_param
     self.slug
